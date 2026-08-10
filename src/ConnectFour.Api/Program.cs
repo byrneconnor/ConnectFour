0015ba4e8f53ca58e1ce48cf7ae4c83a73afa43a
Connor Byrne <byrne.connor@hotmail.com>
2026-08-10 09:05:23 +0100

    Added enpoints and edited Program - dev backend works locally

--- src/ConnectFour.Api/Program.cs
+++ src/ConnectFour.Api/Program.cs
@@ -1,41 +1,29 @@
+using ConnectFour.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<GameStore>();  
+
+const string DevCors = "dev-cors";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(DevCors, policy =>
+        policy.WithOrigins("http://localhost:5173") // Vite's default dev origin
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseCors(DevCors);
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
